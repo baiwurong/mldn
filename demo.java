7fc66028a9a10f391f758642c189917592dfc8c3
baiwurong <baiwurong@gmail.com>
2015-08-31 16:47:47 +0800

    update demo.java

--- demo.java
+++ demo.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String args[]){		
 		system.out.println('hello world');
-		system.out.println('Ô¶³Ì²Ö¿â');
+		system.out.println('哈哈');
 		console.log("haha");
 	}
 }
