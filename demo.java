cf3e496618872d24a9fc405aee3d84c11d2dc503
baiwurong <baiwurong@gmail.com>
2015-08-31 14:36:39 +0800

    Update demo.java
    更新文件

--- demo.java
+++ demo.java
@@ -1,6 +1,7 @@
 public class Hello{
 	public static void main(String args[]){		
 		system.out.println('hello world');
-		system.out.println('Զ�ֿ̲�');
+		system.out.println('Ô¶³Ì²Ö¿â');
+		console.log("haha");
 	}
 }
